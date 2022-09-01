c5e5a57bfbf7508b5326072638359dbfe96a480c
azerose <woojjang8671@naver.com>
2022-09-01 21:20:13 +0900

    apollo를 사용한 서버통신

--- freeboard-frontend/pages/_app.js
+++ freeboard-frontend/pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const client = new ApolloClient({
+    uri: "http://backend09.codebootcamp.co.kr/graphql",
+    cache: new InMemoryCache(),
+  });
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
